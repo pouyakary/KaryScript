7364878252b6878afc4990da78076c6a1f1be57f
Pouya Kary <k@karyfoundation.org>
2017-01-27 18:54:53 +0330

    right test infrastructure

---
+++ tests/grammar/index.js
@@ -0,0 +1,76 @@
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    const pegjs = require('pegjs')
+    const fs    = require('fs')
+    const path  = require('path')
+    const wrap  = require('../libs/wordwrap')
+
+//
+// ─── MAIN DEFS ──────────────────────────────────────────────────────────────────
+//
+
+    function loadTestCases ( ) {
+        const txtLine = '── ────────────────────────────────────────────────────────────────────────────────';
+        const file = fs.readFileSync(
+            path.resolve( path.join( __dirname, './cases.txt' ) ), 'utf8' )
+        return file.split( txtLine ).map( x => x.trim( ) )
+    }
+
+    var testCases = loadTestCases( )
+
+//
+// ─── LOAD PARSER ────────────────────────────────────────────────────────────────
+//
+
+    function loadParser ( ) {
+        const p = path.resolve( path.join( __dirname, '../../src/grammar/grammar.pegjs' ) )
+        const fileContent = fs.readFileSync( p, 'utf8' )
+        return pegjs.generate( fileContent )
+    }
+
+//
+// ─── TESTER ─────────────────────────────────────────────────────────────────────
+//
+
+    module.exports = ( ) => {
+        const parser = loadParser( )
+        const wrapper = wrap( 80 )
+
+        function writeLine ( char ) {
+            let line = [ ]
+            for ( let i = 0; i < 80; i++ ) {
+                line.push( char )
+            }
+            console.log( line.join('') )
+        }
+
+        function test ( code ) {
+            try {
+                parser.parse( code )
+            } catch ( e ) {
+                writeLine('─')
+                console.log( code )
+                writeLine('-')
+                console.log( wrapper( e.message ) )
+                writeLine('─')
+                console.log('\n')
+            }
+        }
+
+        for ( let t of testCases ) {
+            test( t )
+        }
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
