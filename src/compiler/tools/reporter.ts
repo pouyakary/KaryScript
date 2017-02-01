6b9adf99fd091195754657b72ce4a7f2aaec5fd4
Pouya Kary <k@karyfoundation.org>
2017-02-01 19:24:35 +0330

    base of error reporter implemented

---
+++ src/compiler/tools/reporter.ts
@@ -0,0 +1,51 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace KaryScriptCompiler.Reporter {
+
+    //
+    // ─── ERROR INTERFACE ────────────────────────────────────────────────────────────
+    //
+
+        interface CompilerError {
+            message: string
+            kind: ErrorTypes
+            node: AST.IBase | null
+        }
+
+    //
+    // ─── REPORTER STORAGE ───────────────────────────────────────────────────────────
+    //
+
+        let reports = new Array<CompilerError>( )
+
+    //
+    // ─── REPORT TYPES ───────────────────────────────────────────────────────────────
+    //
+
+        export enum ErrorTypes {
+            Grammar, Report, Identifier
+        }
+        
+    //
+    // ─── REPORTER ───────────────────────────────────────────────────────────────────
+    //
+
+        export function Report ( message: string, kind: ErrorTypes, node?: AST.IBase ) {
+            reports.push({
+                message: message,
+                kind: kind,
+                node: ( node )? node : null
+            })
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
