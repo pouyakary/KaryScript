bfeb389318353a532c7f7c716f8adf00f055c9fe
Pouya Kary <k@karyfoundation.org>
2017-02-01 19:42:00 +0330

    deceleration compiler added

---
+++ src/compiler/generators/nodes/statements/deceleration.ts
@@ -0,0 +1,58 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace KaryScriptCompiler.Nodes.Statements.Deceleration {
+
+    //
+    // ─── DECELERATION ───────────────────────────────────────────────────────────────
+    //
+
+        export function Compile ( node: AST.DecelerationStatementBase, env: IEnvInfo ) {
+            if ( node.kind === "SingleAllocInit" )
+                return CompileSingleAllocInit( node as AST.SingleAllocInitDeceleration , env )
+            else
+                return CompileMultiAlloc( node as AST.MultiAllocDeceleration, env )
+        }
+
+    //
+    // ─── SINGLE ALLOC INIT DECELERATION ─────────────────────────────────────────────
+    //
+
+        function CompileSingleAllocInit ( node: AST.SingleAllocInitDeceleration,
+                                           env: IEnvInfo ) {
+
+            const key   = GetDecelerationKey( env )
+            const name  = Expressions.Identifier.Compile( node.assignment.name, env, true )
+            const expr  = CompileSingleNode( node.assignment.value, env )
+
+            return `${ key } ${ name } = ${ expr };`
+        }
+
+    //
+    // ─── MULTI ALLOC DECELERATION ───────────────────────────────────────────────────
+    //
+
+        function CompileMultiAlloc ( node: AST.MultiAllocDeceleration, env: IEnvInfo ) {
+            const key   = GetDecelerationKey( env )
+            const names = node.names.map( x => Expressions.Identifier.Compile( x, env, true ) )
+            return `${ key } ${ names.join(', ') };`
+        }
+
+    //
+    // ─── GET DECELERATION KEY ───────────────────────────────────────────────────────
+    //
+
+        function GetDecelerationKey ( env: IEnvInfo ) {
+            return ( env.ScopeLevel === 0 )? 'var' : 'let'
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
