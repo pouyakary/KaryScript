b2e8a68b54205121562c7ae3d99873ab2173f001
Pouya Kary <k@karyfoundation.org>
2017-02-01 19:18:14 +0330

    identifer complier is done

---
+++ src/compiler/generators/nodes/expressions/identifier.ts
@@ -0,0 +1,47 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace KaryScriptCompiler.Nodes.Expressions.Identifier {
+
+    //
+    // ─── IDENTIFIER ─────────────────────────────────────────────────────────────────
+    //
+
+        export function Compile ( node: AST.IIdentifier, env: IEnvInfo, dec = false ) {
+            // changing the shapes and stuff
+            const name = node.name.replace( /-/g , '_' )
+
+            // in case we are declaring an identifier which is all ready defined
+            if ( dec )
+                if ( DoesTheNameExists( name, env ) )
+                    Reporter.Report(`Identifer "${ node.name }" is already defined.`,
+                                    Reporter.ErrorTypes.Identifier, node )
+
+            // adding the name
+            env.DeclaredIdentifiers.add( name )
+
+            // done
+            return name
+        }
+
+    //
+    // ─── SEARCH FOR IDENTIFIER ──────────────────────────────────────────────────────
+    //
+
+        function DoesTheNameExists ( name: string, env: IEnvInfo ): boolean {
+            for ( let identifier of env.DeclaredIdentifiers )
+                if ( identifier === name )
+                    return true
+            return false
+        }     
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
