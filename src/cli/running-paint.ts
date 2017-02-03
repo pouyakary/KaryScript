fbc8ef05e319deb647979785e10be88cd492e2f2
Pouya Kary <k@karyfoundation.org>
2017-02-04 00:00:08 +0330

    a testing cli for debugging before we write a full one

---
+++ src/cli/running-paint.ts
@@ -0,0 +1,40 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+/// <reference path="../compiler/index.ts" />
+
+namespace KaryScript.CLI {
+
+    //
+    // ─── RUNNING POINT ──────────────────────────────────────────────────────────────
+    //
+
+        export function RunningPoint ( ) {
+            if ( typeof module !== 'undefined' && module.exports )
+                Main( )
+        }
+        
+    //
+    // ─── MAIN ───────────────────────────────────────────────────────────────────────
+    //
+
+        function Main ( ) {
+            // ParseAndRunCommands( )
+            const fs = require('fs')
+            const path = require('path')
+            let content = fs.readFileSync(
+                path.resolve( path.join( __dirname, '../tests/test.kary' ) ), 'utf8' )
+
+            console.log( KaryScript.Compiler.Compile( content ) ) 
+        }
+    
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
