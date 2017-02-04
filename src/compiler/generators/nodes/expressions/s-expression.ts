34d2bf59370b27d8817e397e7e6f729071d69fa2
Pouya Kary <k@karyfoundation.org>
2017-02-04 03:55:15 +0330

    start of s-expressions

---
+++ src/compiler/generators/nodes/expressions/s-expression.ts
@@ -0,0 +1,116 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+/// <reference path="identifier.ts" />
+/// <reference path="address.ts" />
+/// <reference path="../../../tools/reporter.ts" />
+/// <reference path="../../../tools/env.ts" />
+
+
+namespace KaryScript.Compiler.Nodes.SExpression {
+
+    //
+    // ─── S EXPRESSION ───────────────────────────────────────────────────────────────
+    //
+
+        export function Compile ( node: AST.ISExpression, env: IEnvInfo, 
+                                  placeholder?: AST.IBase ) {
+            switch ( node.kind ) {
+                case 'FunctionCallWithArgs':
+                    return CompileFunctionCallWithArgs(
+                        <AST.IFunctionCallWithArgsSExpression> node, env, placeholder
+                    )
+
+                case 'BinaryOperator':
+                    return CompileBinaryOperator(
+                        <AST.IBinaryOperatorSExpression> node, env, placeholder
+                    )
+
+                case 'UnaryOperator':
+                    return ''
+
+                case 'FunctionCallOnly':
+                    return ''
+            }
+        }
+
+    //
+    // ─── FUNCTION CALL WITH ARGS ────────────────────────────────────────────────────
+    //
+
+        function CompileFunctionCallWithArgs ( node: AST.IFunctionCallWithArgsSExpression, 
+                                                env: IEnvInfo, 
+                                       placeholder?: AST.IBase ) {
+            // compiling args
+            let args = new Array<string>( )
+            for ( let arg of node.params ) {
+                if ( arg.type === 'PipePlaceholder' && placeholder )
+                    args.push( Nodes.CompileSingleNode( placeholder, env ) )
+                else
+                    args.push( Nodes.CompileSingleNode( arg, env ) )
+            }
+
+            // and done.
+            return Nodes.CompileSingleNode( node.command, env ) + "(" + 
+                    args.join(', ') + ")" + Env.Semicolon( env )
+        }
+
+    //
+    // ─── BINARY OPERATOR ────────────────────────────────────────────────────────────
+    //
+
+        function CompileBinaryOperator ( node: AST.IBinaryOperatorSExpression, 
+                                          env: IEnvInfo,
+                                  placeholder?: AST.IBase ) {
+
+            const op = TranslateOperator( node.operator )
+
+            function handlePlaceholder ( hand: AST.IBase ) {
+                if ( hand.type === 'PipePlaceholder' && placeholder )
+                    return Nodes.CompileSingleNode( placeholder, env )
+                else
+                    return Nodes.CompileSingleNode( hand, env )
+            }
+
+            return '(' + handlePlaceholder( node.left ) + ' ' + op + ' ' +
+                    handlePlaceholder( node.right ) + ')'
+        }
+
+    //
+    // ─── UNARY OPERATOR ─────────────────────────────────────────────────────────────
+    //
+
+        
+        
+    //
+    // ─── GET OPERATOR ───────────────────────────────────────────────────────────────
+    //
+
+        export function TranslateOperator ( op: string ) {
+            switch ( op ) {
+                case 'sum':         return '+'
+                case 'mul':         return '*'
+                case 'sub':         return '-'
+                case 'div':         return '/'
+                case 'and':         return '&&'
+                case 'or':          return 'or'
+                case 'pow':         return '**'
+                case 'mod':         return '%'
+                case '=':           return '==='
+                case '>':           return '>'
+                case '<':           return '<'
+                case '<=':          return '<='
+                case '!=':          return '!='
+            }
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
