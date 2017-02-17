3a1959f0724d24d579b2357a38c301f61f839d68
Pouya Kary <k@karyfoundation.org>
2017-02-17 23:41:22 +0330

    more build tools

---
+++ build/after-pack.js
@@ -0,0 +1,43 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    const   fs          = require('fs')
+    const   path        = require('path')
+
+//
+// ─── ADDING INFO ────────────────────────────────────────────────────────────────
+//
+
+    const   header      = '//#! /usr/bin/env node\n'
+
+//
+// ─── BIN FILE ───────────────────────────────────────────────────────────────────
+//
+
+    const   binPath     = path.resolve( __dirname, '../bin/kc.js' )
+    let     binFile     = fs.readFileSync( binPath, 'utf8' )
+
+//
+// ─── APPLY HEADER ───────────────────────────────────────────────────────────────
+//
+
+    binFile = header + binFile
+
+//
+// ─── SAVE BIN FILE ──────────────────────────────────────────────────────────────
+//
+
+    fs.writeFileSync( binPath, binFile )
+
+// ────────────────────────────────────────────────────────────────────────────────
--- pack.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    target: 'node',
-    entry: "./bin/compiler.js",
-    output: {
-        path: __dirname + "/bin",
-        filename: "kc.js"
-    }
-};
