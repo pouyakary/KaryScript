a7c48f2df8a632c1776730bb1f71c3d216a903be
Pouya Kary <k@karyfoundation.org>
2017-02-18 02:34:59 +0330

    imports added

---
+++ src/cli/imports.ts
@@ -0,0 +1,24 @@
+
+//
+// Copyright © 2017-present Kary Foundation, Inc. All rights reserved.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace KaryScript.CLI {
+
+    //
+    // ─── IMPORTS ────────────────────────────────────────────────────────────────────
+    //
+
+        export const fs         = require('fs')
+        export const path       = require('path')
+        export const util       = require('util')
+        export const yaml       = require('js-yaml')
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
