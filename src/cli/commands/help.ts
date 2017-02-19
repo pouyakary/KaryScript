8ac09f3ce0d55a29166b9634b307306b2871206a
Pouya Kary <k@karyfoundation.org>
2017-02-19 15:09:34 +0330

    help page completed

--- src/cli/commands/help.ts
+++ src/cli/commands/help.ts
@@ -17,10 +17,46 @@ namespace KaryScript.CLI.Commands {
     //
 
         export function PrintHelp ( ) {
+            console.log( )
+
+            console.log('  KaryScript ' +
+                KaryScript.Compiler.Version.Name + ' (release #' +
+                KaryScript.Compiler.Version.Release + ')')
+
             console.log()
-            console.log('  KaryScript')
-            console.log('  ' + repeat('-', process.env.COLUMNS - 4 ) )
+            console.log('  Command                  Description')
+            console.log( colors.bold( termLine( ) ) )
+            
+            console.log('  --help                   Prints this help page')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+            console.log('  --version                Prints version of currently installed KaryScript')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+            console.log(`  --init                   Initializes a ${
+                colors.underline( 'k.yml' ) } configuration file.`)
+            console.log( colors.bold.grey( termLine( ) ) )
+
+
+            // build way
             console.log()
+            console.log('  Build Option             Description')
+            console.log( colors.bold( termLine( ) ) )
+    
+            console.log('  --srcDir "dir"           Sets root directory for codes to "dir"')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+            console.log('  --files [files]          Adds the [files] array to included files')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+            console.log('  --outDir "dir"           Sets root directory for compilation to "dir"')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+            console.log('  --source-map             Enables source map generation')
+            console.log( colors.bold.grey( termLine( ) ) )
+
+
+            console.log( )
         }
 
     // ────────────────────────────────────────────────────────────────────────────────
